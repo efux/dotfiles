c55ec0bd63ec4c3bcf7a093458e34ad7d349fc0c
Etienne Fux <fux.etienne@gmail.com>
2026-07-01 14:22:10 +0200

    Navigate word by word on Ctrl and arrow key in zsh

--- zsh/.config/zsh/keybindings.zsh
+++ zsh/.config/zsh/keybindings.zsh
@@ -1,4 +1,6 @@
 bindkey '^H' backward-kill-word
+bindkey '^[[1;5C' forward-word
+bindkey '^[[1;5D' backward-word
 
 [[ -n "${key[Home]}" ]] && bindkey "${key[Home]}" beginning-of-line
 [[ -n "${key[End]}"  ]] && bindkey "${key[End]}"  end-of-line
